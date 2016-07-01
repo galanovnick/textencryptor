038965bb0c0d3059b762b8cc9a99d4e7ac71819f
Nikolay Galanov <galanov.nick@gmail.com>
2016-07-01 12:50:34 +0300

    Implemented string converter.

--- src/main/java/encryptor/impl/StringGridConverter.java
+++ src/main/java/encryptor/impl/StringGridConverter.java
@@ -1,11 +1,26 @@
 package encryptor.impl;
 
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class StringGridConverter implements StringConverter {
 
     @Override
     public List<String> transform(List<String> strings) {
-        return null;
+        final List<StringBuilder> resultStrings = new ArrayList<>();
+        for (int i = 0; i < strings.get(0).length(); i++) {
+            resultStrings.add(new StringBuilder());
+        }
+
+        strings.forEach((string) -> {
+            char[] stringChars = string.toCharArray();
+            for (int i = 0; i < stringChars.length; i++) {
+                resultStrings.get(i).append(stringChars[i]);
+            }
+        });
+
+        return Lists.transform(resultStrings, StringBuilder::toString);
     }
 }
--- src/test/java/encryptor/StringGridConverterTest.java
+++ src/test/java/encryptor/StringGridConverterTest.java
@@ -16,10 +16,19 @@ public class StringGridConverterTest {
 
     @Test
     public void testSimpleStringsConverting() {
-        final List<String> strings = new ArrayList<>(Arrays.asList("hav", "ean", "ice", "day"));
+        final List<String> strings = new ArrayList<>(Arrays.asList("have", "anic", "eday"));
 
         final List<String> expected = new ArrayList<>(Arrays.asList("hae", "and", "via", "ecy"));
 
         assertEquals("Converting of simple strings failed.", expected, converter.transform(strings));
+    }
+
+    @Test
+    public void testLastSymbolsConvertion() {
+        final List<String> strings = new ArrayList<>(Arrays.asList("feed", "thed", "og"));
+
+        final List<String> expected = new ArrayList<>(Arrays.asList("fto", "ehg", "ee", "dd"));
+
+        assertEquals("Converting of last symbols failed.", expected, converter.transform(strings));
     }
 }
