99c7436a195a237ec9fc5470aa65e356b8859355
Nikolay Galanov <galanov.nick@gmail.com>
2016-07-01 12:11:05 +0300

    Added tests for text parser.

---
+++ src/main/java/encryptor/impl/TextGridParser.java
@@ -0,0 +1,11 @@
+package encryptor.impl;
+
+import java.util.List;
+
+public class TextGridParser implements TextParser {
+
+    @Override
+    public List<String> analyze(String text) {
+        return null;
+    }
+}
---
+++ src/main/java/encryptor/impl/TextParser.java
@@ -0,0 +1,8 @@
+package encryptor.impl;
+
+import java.util.List;
+
+public interface TextParser {
+
+    List<String> analyze(String text);
+}
